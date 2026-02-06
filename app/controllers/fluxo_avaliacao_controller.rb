9c82638c8246917ce2858180ecac2b5412e55015
MacBookPro <macbookpro@MacBook-Pro-de-MacBookPro.local>
2026-02-06 14:03:17 -0300

    filtrando ocnsultas checklist

--- app/controllers/fluxo_avaliacao_controller.rb
+++ app/controllers/fluxo_avaliacao_controller.rb
@@ -1,5 +1,7 @@
 # app/controllers/fluxo_avaliacao_controller.rb
 class FluxoAvaliacaoController < ApplicationController
+  before_action :set_veiculo, only: [ :avaliar, :salvar_avaliacao ]
+
   def index
     @aguardando_vistoria = GVeiculo
       .left_joins(:g_vistorias_veiculos)
@@ -18,9 +20,51 @@ class FluxoAvaliacaoController < ApplicationController
   end
 
   def avaliar
-    @veiculo  = GVeiculo.find(params[:id])
-    @vistoria = @veiculo.g_vistorias_veiculos.last
-    @avaliacao = GAvaliacaoVeiculo.new(g_veiculo: @veiculo, user_id_avaliador: current_user.id)
-    @pontuacao_checklist = @vistoria.g_checklists_veiculos.sum(:resultado) rescue 0
+    @vistoria = @veiculo.g_vistorias_veiculos.order(created_at: :desc).first
+
+    unless @vistoria&.g_checklists_veiculos&.any?
+      redirect_to fluxo_avaliacao_path, alert: "Este veículo ainda não possui checklist preenchido."
+      return
+    end
+
+    @avaliacao = GAvaliacaoVeiculo.new(
+      g_veiculo: @veiculo,
+      user_id_avaliador: current_user.id
+    )
+
+    @pontuacao_checklist = @vistoria.g_checklists_veiculos.sum("CAST(resultado AS numeric)").to_f
+  end
+
+  def salvar_avaliacao
+    @avaliacao = GAvaliacaoVeiculo.new(avaliacao_params.merge(
+      g_veiculo: @veiculo,
+      user_id_avaliador: current_user.id,
+      avaliado_em: Time.current
+    ))
+
+    if @avaliacao.save
+      redirect_to fluxo_avaliacao_path, notice: "Avaliação registrada com sucesso."
+    else
+      @vistoria = @veiculo.g_vistorias_veiculos.order(created_at: :desc).first
+      @pontuacao_checklist = @vistoria&.g_checklists_veiculos&.sum("CAST(resultado AS numeric)").to_f
+      render :avaliar, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_veiculo
+    @veiculo = GVeiculo.find(params[:id])
+  end
+
+  def avaliacao_params
+    params.require(:g_avaliacao_veiculo).permit(
+      :valor_mercado,
+      :valor_referencia_fipe,
+      :percentual_depreciacao,
+      :valor_depreciado,
+      :valor_inicial_lance,
+      :pontuacao_checklist
+    )
   end
 end
