366620ee240f42ced642f76a01b8d187645e9a09
BilionBoy <dhiogoandre26@gmail.com>
2026-02-06 02:25:37 -0400

    refactor: ajustes for m tipos veiculos

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,7 @@
 
 Rails.application.routes.draw do
   resources :g_tipos_veiculos
-  resources :g_status_leiloes_veiculos
-  resources :g_status_veiculos
-  resources :g_distritos
-  resources :g_municipios
+
   devise_for :users
 
   root "home#index"
@@ -14,6 +11,10 @@ Rails.application.routes.draw do
   # Recursos principais
   resources :g_estados
   resources :g_paises
+  resources :g_status_leiloes_veiculos
+  resources :g_status_veiculos
+  resources :g_distritos
+  resources :g_municipios
 
   get "up" => "rails/health#show", as: :rails_health_check
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2026_02_06_061340) do
+ActiveRecord::Schema[7.2].define(version: 2026_02_06_062141) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -74,6 +74,15 @@ ActiveRecord::Schema[7.2].define(version: 2026_02_06_061340) do
   end
 
   create_table "g_status_veiculos", force: :cascade do |t|
+    t.string "descricao"
+    t.string "created_by"
+    t.string "updated_by"
+    t.datetime "deleted_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "g_tipos_veiculos", force: :cascade do |t|
     t.string "descricao"
     t.string "created_by"
     t.string "updated_by"
