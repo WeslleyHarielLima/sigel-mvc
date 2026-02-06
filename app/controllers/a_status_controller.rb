ca5b2f25239316b744c5bae4d64876cd9d0c2b00
BilionBoy <dhiogoandre26@gmail.com>
2026-02-06 03:08:56 -0400

    refactor: index, upcases, downcases e concatenacao

--- app/controllers/a_status_controller.rb
+++ app/controllers/a_status_controller.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 class AStatusController < ApplicationController
   before_action :set_a_status, only: %i[show edit update destroy]
 
@@ -20,7 +21,7 @@ class AStatusController < ApplicationController
     @a_status = AStatus.new(a_status_params)
 
     if @a_status.save
-      redirect_to a_status_path, notice: t('messages.created_successfully')
+      redirect_to a_status_index_path, notice: t("messages.created_successfully")
     else
       render :new, status: :unprocessable_entity
     end
@@ -28,7 +29,7 @@ class AStatusController < ApplicationController
 
   def update
     if @a_status.update(a_status_params)
-      redirect_to a_status_path, notice: t('messages.updated_successfully'), status: :see_other
+      redirect_to a_status_index_path, notice: t("messages.updated_successfully"), status: :see_other
     else
       render :edit, status: :unprocessable_entity
     end
@@ -36,25 +37,25 @@ class AStatusController < ApplicationController
 
   def destroy
     if @a_status.destroy
-      redirect_to a_status_url, notice: t('messages.deleted_successfully')
+      redirect_to a_status_url, notice: t("messages.deleted_successfully")
     else
-      redirect_to a_status_url, alert: t('messages.delete_failed_due_to_dependencies')
-    end   
+      redirect_to a_status_url, alert: t("messages.delete_failed_due_to_dependencies")
+    end
   end
 
   private
 
   def set_a_status
     @a_status = AStatus.find_by(id: params[:id])
-    return redirect_to a_status_path, alert: t('messages.not_found') unless @a_status
+    redirect_to a_status_index_path, alert: t("messages.not_found") unless @a_status
   end
 
   def a_status_params
-    permitted_attributes = AStatus.column_names.reject { |col| ['deleted_at', 'created_by', 'updated_by'].include?(col) }
+    permitted_attributes = AStatus.column_names.reject { |col| [ "deleted_at", "created_by", "updated_by" ].include?(col) }
     params.require(:a_status).permit(permitted_attributes.map(&:to_sym))
   end
 
   def handle_not_found
-    redirect_to a_status_path, alert: t('messages.not_found')
+    redirect_to a_status_index_path, alert: t("messages.not_found")
   end
 end
